3bdcbb9b71797d5056e0b54d833df0c3f3694ad7
Grigory Panov <gripanov@gmail.com>
2017-07-11 15:11:33 +0300

    initial project version

---
+++ src/ru/gpanov/Main.java
@@ -0,0 +1,11 @@
+package ru.gpanov;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Speaker speaker = new Speaker();
+	    speaker.sayHello();
+    }
+
+
+}
---
+++ src/ru/gpanov/Speaker.java
@@ -0,0 +1,10 @@
+package ru.gpanov;
+
+/**
+ * Created by user on 7/11/2017.
+ */
+public class Speaker {
+    public void sayHello(){
+        System.out.println("Say Hello");
+    }
+}
