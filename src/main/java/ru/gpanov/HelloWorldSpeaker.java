03fd24eacc05d65f5a125b8a0f90271406b58385
Grigory Panov <gripanov@gmail.com>
2017-08-24 13:27:21 +0300

    Prepare Sample

---
+++ src/main/java/ru/gpanov/HelloWorldSpeaker.java
@@ -0,0 +1,9 @@
+package ru.gpanov;
+
+import java.util.Vector;
+
+public class HelloWorldSpeaker implements {
+    public void sayHello(){
+        System.out.println("Hello World!");
+    } 
+}
--- src/main/java/ru/gpanov/Main.java
+++ src/main/java/ru/gpanov/Main.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	    Speaker speaker = new Speaker();
-	    speaker.sayHello();
+	    HelloWorldSpeaker speaker = new Speaker();
+	    speaker.say();
     }
 }
